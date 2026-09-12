7753dadb93f69cf42d1a95b576ab9e7fad32a944
Evgeness <zenasabanov1@gmail.com>
2026-09-12 12:21:13 +0300

    fix: исправление ошибки при запуске сайта

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,3 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
@@ -13,9 +9,6 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
     <ThemeProvider theme={theme}>
       <CssBaseline />
         <BrowserRouter>
